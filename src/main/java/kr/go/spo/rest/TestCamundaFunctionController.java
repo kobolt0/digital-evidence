ddeba335dc0caaecf66d2cc2f1d5c057bf8cfe27
dongchun <kobolt0@hanmail.net>
2022-06-28 13:36:01 +0900

    swagger 적용

--- src/main/java/kr/go/spo/rest/TestCamundaFunctionController.java
+++ src/main/java/kr/go/spo/rest/TestCamundaFunctionController.java
@@ -5,12 +5,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.ProcessEngines;
 import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.engine.impl.context.BpmnExecutionContext;
+import org.camunda.bpm.engine.impl.context.Context;
+import org.camunda.bpm.engine.impl.persistence.entity.ExecutionEntity;
 import org.camunda.bpm.engine.runtime.ProcessInstanceWithVariables;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,21 +28,32 @@ import java.util.Map;
 public class TestCamundaFunctionController {
   private SqlSessionTemplate sqlSessionTemplate;
 
+  // 프로세스 시작
   @GetMapping("/startPorc")
-  public String testJsonObj() {
-
-
-
+  public String startPorc(HttpServletRequest req) {
+    log.debug("##@# REST. {}", req.getRequestURL());
     Map<String, Object> variables=new HashMap<>();
-    variables.put("nombrePdf","nombrePdf");
+    variables.put("caseId","nombrePdf");
     variables.put("datos","datos");
 
     ProcessEngine processEngine = ProcessEngines.getDefaultProcessEngine();
     RuntimeService runtimeService=processEngine.getRuntimeService();
 
-    ProcessInstanceWithVariables instance = runtimeService.createProcessInstanceByKey("POCFirma")
+    
+
+    ProcessInstanceWithVariables instance = runtimeService.createProcessInstanceByKey("preprocess")
             .setVariables(variables)
             .executeWithVariablesInReturn();
+    return null;
+  }
+
+  // 진행중인 프로세스 접근
+
+  @GetMapping("/runningProc")
+  public String runningProc(HttpServletRequest req) {
+    log.debug("##@# REST. {}", req.getRequestURL());
+    BpmnExecutionContext con = Context.getBpmnExecutionContext();
+    ExecutionEntity inst = Context.getBpmnExecutionContext().getProcessInstance();
     return null;
   }
 
